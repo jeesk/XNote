3306db2366ec1f58910d449b3c9709f5b9fa98d4
song <songqifu@linakesi.com>
2023-09-23 23:51:21 +0800

    移除不必要的内容

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,23 +1,37 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>记录</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">记录</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-list>
+                <ion-item>
+                    <ion-label>Pokémon Yellow</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Mega Man X</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>The Legend of Zelda</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Pac-Man</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Super Mario World</ion-label>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
 </script>
